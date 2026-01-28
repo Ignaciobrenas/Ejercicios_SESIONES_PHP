011c069607d1a56f88d56d1f7476be92a965935b
Ignacio <ignaciobrenas@gmail.com>
2026-01-28 10:17:03 +0100

    Ejercicio 1 Estructura

---
+++ Ejercicio1.php
@@ -0,0 +1,39 @@
+// ary10,20,30
+
+// to process the form
+// if req es post
+
+// when user click on a button
+// detect button (modify or average)
+
+// modify logic
+if isset(modify)
+// get form data
+//$number = POST["value"]
+//$position = POST["position"]
+
+// modify selected position
+//array[position] = number;
+
+// average logic
+// calculate the average value (sum all values / number of values)
+// for array
+// total += pos
+
+//$avg = total/count(array)
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
