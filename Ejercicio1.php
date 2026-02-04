6e62fabac78bdc782c78a7b8deb9cc1c77366e35
Ignacio <ignaciobrenas@gmail.com>
2026-02-04 10:32:01 +0100

    Ejercicio 1 (Final)

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -2,17 +2,34 @@
 // Start the session
 session_start();
 // ary10,20,30
+if (!isset($_SESSION['numbers'])) {
+    $_SESSION['numbers'] = array(10, 20, 30);
+}
 
 // to process the form
 // if req es post
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 // when user click on a button
-// detect button (modify or average)
 
+// reeset logic
+if (isset($_POST['reset'])) {
+        $_SESSION['numbers'] = array(10, 20, 30);
+        unset($_SESSION['average']);
+    }
 // modify logic
 //if isset(modify)
+if (isset($_POST['modify'])) {
+        $position = $_POST['position'];
+        $number = $_POST['value'];
+
+        if ($position !== "" && $number !== "") {
+            $_SESSION['numbers'][$position] = $number;
+        }
+    }
 // get form data
 //$number = POST["value"]
+
 //$position = POST["position"]
 
 // modify selected position
@@ -23,40 +40,53 @@ session_start();
 // for array
 // total += pos
 
+if (isset($_POST['average'])) {
+        $total = 0;
 
-//$avg = total/count(array)
+        foreach ($_SESSION['numbers'] as $num) {
+            $total += $num;
+        }
+
+        $_SESSION['average'] = $total / count($_SESSION['numbers']);
+    }
 
 
+
+    
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1</title>
-
 </head>
 
 <body>
     <h1>Ejercicio 1 - Modificar arrays</h1>
-    <form method="post" enctype="multipart/form-data">
+    <form method="post">
         Posicion a Modificar:
-        <select name="extension">
-            <option value="">--Selecciona--</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-
+        <select name="position">
+            <option value="">Selecciona</option>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
         </select>
         <br><br>
-        Nuevo Valor: <input type="number" name="tamano" min="1"><br><br>
-        <input type="submit" value="MODIFY">
-        <input type="submit" value="AVERAGE">
-        <input type="submit" value="RESET">
+        Nuevo Valor: <input type="number" name="value" min="1"><br><br>
+        <input type="submit" name="modify" value="MODIFICAR">
+        <input type="submit" name="average" value="HACER LA MEDIA">
+        <input type="submit" name="reset" value="RESET">
     </form>
 
+    <p>ARRAY ACTUAL: <?php echo implode(", ", $_SESSION['numbers']); ?></p>
+
+    <?php if (isset($_SESSION['average'])) { ?>
+        <p>MEDIA DEL ARRAY: <?php echo $_SESSION['average']; ?></p>
+    <?php } ?>
 
 </body>
 
